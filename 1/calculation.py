4286e7e6ace3dd642ee2eddd44ded574bcd49d38
Luke <lukefonfabre334@gmail.com>
2024-12-27 15:40:44 +0900

    算術演算子まとめ

---
+++ 1/calculation.py
@@ -0,0 +1,20 @@
+# print(7-2)
+# print(7*2)
+# #7の2乗
+# print(7**2)
+# print(7/2)
+# #floor関数みたいな感じで小数を切り落とす
+# print(7//2)
+# #7mod2
+# print(7%2)
+
+# #途中でもimportはできる
+import math
+# print(math.sqrt(2))
+# print(math.pi)
+
+# print(math.sin(math.pi/2))
+# print(math.sin(math.pi/4))
+
+# 練習問題
+print((math.sqrt(5)+1)/2)
